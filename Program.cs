66fdb8625632ea333e7bafa103bf1c0de1302988
user <user@LAPTOP-2DK3J1EI>
2023-09-04 18:03:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Ingrese categoría del hotel:");
+Console.WriteLine("(3) 3 estrellas");
+Console.WriteLine("(5) 5 estrellas");
+int categ = int.Parse(Console.ReadLine());
+Console.Write("Ingrese días de estadia: ");
+int dias = int.Parse(Console.ReadLine());
+float precio = 0.00f;
+switch (categ)
+{
+    case 3:
+        if (dias <= 5)
+        {
+            precio = 100.00f * dias;
+
+        } else if (dias <= 10) {
+            precio = 85.00f * dias;
+        } else if ((dias >= 11))
+        {
+            precio = 70.00f * dias;
+        }
+        break;
+    case 5:
+        if (dias <= 5)
+        {
+            precio = 300.00f * dias;
+
+        }
+        else if (dias <= 10)
+        {
+            precio = 270.00f * dias;
+        }
+        else if ((dias >= 11))
+        {
+            precio = 250.00f * dias;
+        }
+        break;
+}
+precio += dias * 7.00f;
+Console.WriteLine("el precio sería: " +precio);
